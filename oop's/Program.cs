2ae1bbdde0cbdcf7f79a2393eaf7067d474b7654
umernadim <itsumernadeem@gmail.com>
2025-10-22 12:52:41 +0500

    Add project files.

---
+++ oop's/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// *******************************
+//      Classes and OOP's
+// *******************************
+
+// 1st method of object initialization 
+//Person person1 = new();
+//person1.Name = "umer";
+//person1.Age = 23;
+//person1.Gender = "male";
+//person1.Profession = "Wev developer";
+
+// 2nd method of object initialization   
+//Person person1 = new()
+//{
+//    Name = "umer",
+//    Age = 23,
+//    Gender = "male",
+//    Profession = "Web developer"
+//};
+
+//Console.WriteLine($"Name: {person1.Name}, Age: {person1.Age}, Gender: {person1.Gender}, Profession: {person1.Profession}");
+
+
+// Creating class name Person
+//class Person
+//{
+//    public string Name;
+//    public int Age;
+//    public string Gender;
+//    public string Profession;
+//}
+
+
+
+//List<Mobile> mobiles = new()
+//{
+//    new Mobile()
+//    {
+//        brand = "Samsung",
+//        model = "Galaxy S21",
+//        price = 799
+//    },
+//    new Mobile()
+//    {
+//        brand = "Apple",
+//        model = "iPhone 13",
+//        price = 999
+//    },
+//    new Mobile()
+//    {
+//        brand = "Google",
+//        model = "Pixel 6",
+//        price = 599
+//    }
+//};
+
+//foreach(var mobile in mobiles)
+//{
+//    Console.WriteLine($"Brand: {mobile.brand}, Model: {mobile.model}, Price: {mobile.price}");
+//}
+
+//class Mobile
+//{
+//    public string brand;
+//    public string model;
+//    public int price;
+//}
