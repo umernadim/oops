4fbf2cd6f5135d4037ca0cf99152592d49f7e804
umernadim <itsumernadeem@gmail.com>
2025-10-23 21:54:45 +0500

    new tasks

--- oop's/Program.cs
+++ oop's/Program.cs
@@ -122,3 +122,82 @@
 //}
 
 
+
+
+// Creating list of objects using constructor
+//List<Student> student = new()
+//{
+//    new Student("Umer", 04),
+//    new Student("Usman", 05)
+//};
+
+//foreach (var st in student)
+//{
+//Console.WriteLine($"studentName: {st.name}, rollNo: {st.rollNo}");   
+//}
+
+//class Student
+//{
+//    public string name;
+//    public int rollNo;
+//    public Student(string name, int rollNo)
+//    {
+//        this.name = name;
+//        this.rollNo = rollNo;
+//    }
+//}
+
+
+//Product Details 
+//List<Product> prducts = new()
+//{
+//    new Product("Laptop", 80000, 5),
+//    new Product("Mouse", 250, 8),
+//    new Product("Keyboard", 1500, 3)
+//};
+
+//foreach(var product in prducts)
+//{
+//    Console.WriteLine($"Product Name: {product.name}, Price: {product.price}, Quantity: {product.quantity}");
+//}
+//class Product
+//{
+//    public string name;
+//    public int price;
+//    public int quantity; 
+//    public Product(string name, int price, int quantity)
+//    {
+//        this.name = name;
+//        this.price = price;
+//        this.quantity = quantity;
+//    }
+//}
+//
+
+
+// Student Marks
+List<Students> studets = new()
+{
+    new Students("umer", 73, 67, 90),
+    new Students("danish", 65, 78, 89),
+    new Students("usman", 88, 70, 89)
+};
+
+foreach(var student in studets)
+{
+    Console.WriteLine($"stName: {student.name}, engMarks: {student.engMarks}, urduMarks: {student.urduMarks}, sciMarks: {student.sciMarks}");
+}
+class Students
+{
+    public string name;
+    public int engMarks;
+    public int urduMarks;
+    public int sciMarks;
+    public Students(string name, int engMarks, int urduMarks, int sciMarks)
+    {
+        this.name = name;
+        this.engMarks = engMarks;
+        this.urduMarks = urduMarks;
+        this.sciMarks = sciMarks;
+    }
+}
